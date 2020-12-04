59a130aea77353be1eed3678a808c3b09ff04d06
Angel Cascarino <angel.cascarino@googlemail.com>
2020-12-04 22:42:23 +0000

    Solved day 2 but staged all the changes this time

--- day2/part1.py
+++ day2/part1.py
@@ -1,10 +1,15 @@
-import numpy as np
-
 with open("day2/input.txt", "r") as f:
     input_file = [tuple(l.split()) for l in f.readlines()]
 
+counter = 0
+
 for entry in input_file:
     schema, letter, password = entry
-    
     lowbound, highbound = schema.split('-')
     letter = letter[0]
+    
+    occurances = password.count(letter)
+    if int(lowbound) <= occurances and occurances <= int(highbound):
+        counter += 1
+
+print(counter)
---
+++ day2/part2.py
@@ -0,0 +1,18 @@
+with open("day2/input.txt", "r") as f:
+    input_file = [tuple(l.split()) for l in f.readlines()]
+
+counter = 0
+
+for entry in input_file:
+    schema, letter, password = entry
+
+    first_position, second_position = schema.split('-')
+    first_position = int(first_position) - 1
+    second_position = int(second_position) - 1
+
+    letter = letter[0]
+
+    if bool(password[first_position] == letter) ^ bool(password[second_position] == letter):
+        counter += 1
+
+print(counter)
