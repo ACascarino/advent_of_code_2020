209f53af590a6cafec828c8937481a9344ab812b
Angel Cascarino <angel.cascarino@googlemail.com>
2020-12-04 15:30:55 +0000

    Finished D1:P1

--- day1/part1.py
+++ day1/part1.py
@@ -1,5 +1,22 @@
-import numpy
+import numpy as np
 
 with open("input.txt", "r") as f:
-    input_list = sorted(map(float, f.readlines()))
-    
+    input_list = tuple(map(float, f.readlines()))
+
+small_list = iter(sorted(input_list))
+large_list = iter(sorted(input_list, reverse=True))
+
+small_elem = next(small_list)
+large_elem = next(large_list)
+
+while True:
+    sum_elem = small_elem + large_elem
+    if sum_elem > 2020:
+        large_elem = next(large_list)
+    elif sum_elem < 2020:
+        small_elem = next(small_list)
+    else:
+        target_pair = (small_elem, large_elem)
+        break
+
+print(np.prod(target_pair))
